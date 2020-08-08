73f08f1a26c6ffd48f306db6f9771d9ce612f732
Kamil Bogucki <boguckikamil@hotmail.com>
2020-08-09 00:21:21 +0200

    finish chat multiple clients

--- client/app.js
+++ client/app.js
@@ -1,3 +1,5 @@
+const socket = io();
+
 const loginForm = document.getElementById('welcome-form')
 const messagesSection = document.getElementById('messages-section');
 const messagesList = document.getElementById('messages-list');
@@ -6,29 +8,51 @@ const userNameInput = document.getElementById('username')
 const messageContentInput = document.getElementById('message-content')
 let userName = '';
 
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('newUser', (user) => addMessage('Chat bot', `${user} has joined the chat`))
+socket.on('userLeft', (user) => addMessage('Chat bot', `${user} has left the chat`))
+
 function login(event) {
 
     event.preventDefault();
-
-    if (userNameInput.value === userName) {
+    if (!userNameInput.value) {
         alert('sorry bro, but you forgot something')
     }
 
     else {
+        userName = userNameInput.value
         loginForm.classList.remove('show');
         messagesSection.classList.add('show');
+        socket.emit('join', userName);
     }
 };
 
 function sendMessage(event) {
     event.preventDefault();
+
+    let messageContent = messageContentInput.value;
+    if (!messageContent.length) {
+        alert('You have to type something!');
+    }
+    else {
+        addMessage(userName, messageContent);
+        socket.emit('message', { author: userName, content: messageContent })
+        messageContentInput.value = '';
+    }
 }
 
 function addMessage(author, content) {
     const message = document.createElement('li');
     message.classList.add('message');
     message.classList.add('message--received');
-    if (author === userName) message.classList.add('message--self');
+    if (author === userName) { 
+        message.classList.add('message--self');
+    }
+
+    if(author === 'Chat bot') {
+        message.style.fontStyle = 'italic';
+    }
+    
     message.innerHTML = `
       <h3 class="message__author">${userName === author ? 'You' : author}</h3>
       <div class="message__content">
@@ -43,12 +67,7 @@ loginForm.addEventListener('submit', function (event) {
 });
 
 addMessageForm.addEventListener('submit', function (event) {
-    if (messageContentInput.value === '') {
-        alert('Hej, kolego złoty. Napisz coś')
-    }
-    else {
-        addMessage(userName, messageContentInput.value);
-        sendMessage(event);
-    }
-
+    sendMessage(event);
 });
+
+// console.log(userName);
--- server.js
+++ server.js
@@ -1,15 +1,53 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const app = express();
+const socket = require('socket.io');
 
-const messages = [];
+let messages = [];
+let users = [];
 
 app.use(express.static(path.join(__dirname + '/client')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/index.html'));
+  res.sendFile(path.join(__dirname + '/client/index.html'));
+});
+
+const server = app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+  console.log('New client! Its id – ' + socket.id);
+
+  socket.on('message', (message) => {
+    console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+    console.log(users);
   });
 
-app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
+  socket.on('join', (user) => {
+    users.push({ name: user, id: socket.id });
+    socket.broadcast.emit('newUser', user);
   });
+
+  socket.on('disconnect', (user) => { 
+
+    users.filter(activeUsers => {
+      if (activeUsers.id === socket.id) {
+        user = activeUsers.name;
+      }
+    });
+    
+    socket.broadcast.emit('userLeft', user);
+
+    console.log('Oh, socket ' + socket.id + ' has left') });
+
+
+
+    // users = users.filter(activeUsers => activeUsers.id !== socket.id);
+
+  console.log('I\'ve added a listener on message event \n');
+});
